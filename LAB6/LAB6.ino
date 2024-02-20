59d811cb3e7044c0851174e26958ae11d267ca5e
HarlanCook <81379974+HarlanCook@users.noreply.github.com>
2024-02-20 15:53:10 -0600

    Initial commit, pulse sensor reading

--- LAB6/LAB6.ino
+++ LAB6/LAB6.ino
@@ -1,4 +1,12 @@
-const int sensor_pin = 0;
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+unsigned long pulse_period = 0;
+int upper_threshold = 800;
+int lower_threshold = 450;
+
+const int sensor_pin = A0;
 int pulse_signal = 0;
 float BPM = 0;
 
@@ -10,5 +18,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   pulse_signal = analogRead(sensor_pin);
-  Serial.println(pulse_signal);
+  //Serial.println(pulse_signal);
+  delay(50);
+
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    // Do something about this peak
+  }
+
+  if (pulse_signal < lower_threshold) {
+    any_peak_detected = false;
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+      } 
+    else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      }
+  }
+  
+  Serial.println(pulse_period);
 }
